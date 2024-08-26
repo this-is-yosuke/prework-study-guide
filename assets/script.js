70004092949800154257818a45e9b3153123c9f3
this-is-yosuke <yosuke.kibe@gmail.com>
2024-08-26 05:14:27 -0600

    added a for loop

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(let x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+};
+
+// if (topic === 'HTML') {
+//     console.log("Let's study HTML!");
+// }else if(topic === 'CSS'){
+//     console.log("Let's study CSS!");
+// }else if(topic === 'Git'){
+//     console.log("Let's study Git!");
+// }else if(topic === 'Javascript'){
+//     console.log("Let's study Javascript!");
+// }else{
+//     console.log('Please try again!');
+// }
+
+// Looping demonstration
+// const shapes = ["triangle", "square", "pentagon", "circle"];
+// for(let x = 0; x < shapes.length; x++) {
+//     console.log(shapes[x]);
+// }
